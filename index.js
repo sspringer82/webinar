e56ee1e7b11f49b9ffcddf38ab68c5ce9b4c66b6
Sebastian Springer <88656+sspringer82@users.noreply.github.com>
2022-08-24 09:22:59 +0200

    dotenv

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ import express from 'express';
 import userRouter from './users/index.js';
 import authRouter, { validateToken } from './auth/index.js';
 import middleware from './middleware.js';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const port = process.env.PORT || 8080;
 
