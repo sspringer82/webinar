9faad267ec67bd914056f8354d40dd9ad0160091
Sebastian Springer <88656+sspringer82@users.noreply.github.com>
2022-08-31 12:21:27 +0200

    configuration

--- user-app/src/app.module.ts
+++ user-app/src/app.module.ts
@@ -5,19 +5,25 @@ import { AppService } from './app.service';
 import { User } from './users/user.entity';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
     UsersModule,
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: 'localhost',
-      port: 3306,
-      username: 'root',
-      password: 'geheim',
-      database: 'users',
-      entities: [User],
-      synchronize: false,
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        type: 'mysql',
+        host: configService.get<string>('DATABASE_HOST'),
+        port: configService.get<number>('DATABASE_PORT'),
+        username: configService.get<string>('DATABASE_USER'),
+        password: configService.get<string>('DATABASE_PASS'),
+        database: configService.get<string>('DATABASE_DB'),
+        entities: [User],
+        synchronize: false,
+      }),
+      inject: [ConfigService],
     }),
     AuthModule,
   ],
--- user-app/src/app.service.ts
+++ user-app/src/app.service.ts
@@ -1,8 +1,10 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class AppService {
+  constructor(private readonly configService: ConfigService) {}
   getHello(): string {
-    return 'Hello World!';
+    return `Hello ${this.configService.get<string>('DATABASE_USER')}`;
   }
 }
